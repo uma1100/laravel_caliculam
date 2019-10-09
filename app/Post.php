49531af1bce0758911283851028a19de17d76dd6
accaxx <a.asahara22@gmail.com>
2019-10-10 00:19:24 +0900

    [add]limit

--- app/Post.php
+++ app/Post.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    const MAX_COUNT_GET_POSTS = 10;
+
     public function getAll()
     {
-        return $this->get();
+        return $this->limit(self::MAX_COUNT_GET_POSTS)->get();
     }
 }
