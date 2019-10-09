bf66cbbb99c980c9739e9abd15c0b408333d2611
accaxx <a.asahara22@gmail.com>
2019-10-10 00:05:30 +0900

    [add]GET All Posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post as PostModel;
+
+class PostController extends Controller
+{
+    
+    public function __construct(PostModel $post_model)
+    {
+        $this->post_model = $post_model;
+    }
+
+    /**
+     * post一覧を表示する
+     *
+     * @return Reposnse index
+     */
+    public function index()
+    {
+        return view('posts/index')->with(['posts' => $this->post_model->getAll()]);
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1 class="title">
+            Hello This is Laraposts;
+        </h1>
+
+        <div class="content">
+            <div>
+            @foreach ($posts as $post)
+              <h3>{{ $post->title }}</h3>
+              <p>{{ $post->body }}</p>
+            @endforeach
+        </div>
+    </body>
+</html>
