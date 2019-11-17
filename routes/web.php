27039cf9c99491d99fed280e9e35d4289ab51b92
accaxx <a.asahara22@gmail.com>
2019-11-17 22:50:01 +0900

    [add]create/show

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-
 Route::get('/', 'PostController@index');
-// @FIXME 
 Route::get('/posts', 'PostController@index');
+Route::get('/posts/{id}', 'PostController@show');
+Route::get('/posts/create', 'PostController@getCreate');
+Route::post('/posts/create', 'PostController@postCreate');
